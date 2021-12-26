0ac248f5a3f071e952c148a4ba8a1450e9c4a393
MKTunca <91753517+MKTunca@users.noreply.github.com>
2021-12-27 00:22:48 +0300

    Add files via upload

---
+++ WindowsFormsApp16/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace WindowsFormsApp16
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Y1 = new System.Windows.Forms.Label();
+            this.Y2 = new System.Windows.Forms.Label();
+            this.Y3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.T1 = new System.Windows.Forms.Timer(this.components);
+            this.T2 = new System.Windows.Forms.Timer(this.components);
+            this.T3 = new System.Windows.Forms.Timer(this.components);
+            this.sonuclar = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // Y1
+            // 
+            this.Y1.BackColor = System.Drawing.Color.Red;
+            this.Y1.ForeColor = System.Drawing.Color.White;
+            this.Y1.Location = new System.Drawing.Point(0, 36);
+            this.Y1.Name = "Y1";
+            this.Y1.Size = new System.Drawing.Size(70, 16);
+            this.Y1.TabIndex = 0;
+            this.Y1.Text = "yarışmacı1";
+            // 
+            // Y2
+            // 
+            this.Y2.BackColor = System.Drawing.Color.Blue;
+            this.Y2.ForeColor = System.Drawing.Color.White;
+            this.Y2.Location = new System.Drawing.Point(0, 75);
+            this.Y2.Name = "Y2";
+            this.Y2.Size = new System.Drawing.Size(70, 16);
+            this.Y2.TabIndex = 1;
+            this.Y2.Text = "yarışmacı2";
+            // 
+            // Y3
+            // 
+            this.Y3.BackColor = System.Drawing.Color.Purple;
+            this.Y3.ForeColor = System.Drawing.Color.White;
+            this.Y3.Location = new System.Drawing.Point(0, 110);
+            this.Y3.Name = "Y3";
+            this.Y3.Size = new System.Drawing.Size(70, 16);
+            this.Y3.TabIndex = 2;
+            this.Y3.Text = "yarışmacı3";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Location = new System.Drawing.Point(600, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(10, 259);
+            this.panel1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(697, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "yarış";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // T1
+            // 
+            this.T1.Tick += new System.EventHandler(this.T1_Tick);
+            // 
+            // T2
+            // 
+            this.T2.Tick += new System.EventHandler(this.T2_Tick);
+            // 
+            // T3
+            // 
+            this.T3.Tick += new System.EventHandler(this.T3_Tick);
+            // 
+            // sonuclar
+            // 
+            this.sonuclar.FormattingEnabled = true;
+            this.sonuclar.Location = new System.Drawing.Point(652, 110);
+            this.sonuclar.Name = "sonuclar";
+            this.sonuclar.Size = new System.Drawing.Size(120, 95);
+            this.sonuclar.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 261);
+            this.Controls.Add(this.sonuclar);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.Y3);
+            this.Controls.Add(this.Y2);
+            this.Controls.Add(this.Y1);
+            this.Name = "Form1";
+            this.Text = "yarış simülasyonu";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Y1;
+        private System.Windows.Forms.Label Y2;
+        private System.Windows.Forms.Label Y3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer T1;
+        private System.Windows.Forms.Timer T2;
+        private System.Windows.Forms.Timer T3;
+        private System.Windows.Forms.ListBox sonuclar;
+    }
+}
+
---
+++ WindowsFormsApp16/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp16
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        Random rd = new Random();
+        private void T1_Tick(object sender, EventArgs e)
+        {
+            int solY1 = Y1.Left;
+            int r = rd.Next(solY1, solY1 + 60);
+            Y1.Left = r;
+            if(Y1.Left+70>=600)
+            {
+                T1.Enabled = false;
+                T1.Stop();
+                sonuclar.Items.Add("yarışmacı 1");
+            }
+           
+
+
+        }    
+
+        
+        private void T2_Tick(object sender, EventArgs e)
+        {
+            int solY2 = Y2.Left;
+            int r = rd.Next(solY2, solY2 + 60);
+            Y2.Left = r;
+            if(solY2+70>=600)
+            {
+                T2.Enabled = false;
+                T2.Stop();
+                sonuclar.Items.Add("yarışmacı 2");
+            }
+             
+
+
+
+
+        }
+       
+        private void T3_Tick(object sender, EventArgs e)
+        {
+            int solY3 = Y3.Left;
+            int r = rd.Next(solY3, solY3 + 60);
+            Y3.Left = r;
+            if (solY3 + 70 >= 600)
+            {
+                T3.Enabled = false;
+                T3.Stop();
+                sonuclar.Items.Add("yarışmacı 3");
+            }
+           
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            T1.Enabled = true;
+            T1.Start();
+            T2.Enabled = true;
+            T2.Start();
+            T3.Enabled = true;
+            T3.Start();
+        }
+    }
+}
